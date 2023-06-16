496862b28658cb67a946a524187f6ae9594b5b59
Sebastián Aedo <me@etra0.lat>
2023-06-15 20:20:12 -0400

    Fixed specs to the new API

--- spec/charqui_spec.cr
+++ spec/charqui_spec.cr
@@ -4,18 +4,37 @@ describe Charqui do
   it "Should parse ratio correctly" do
     cli = Charqui::CLI.new
     cli.parse_ratio "1:1"
-    cli.ratio.should eq(0.5)
+    cli.ratio.should be_close(0.5, 1e-3)
 
     cli.parse_ratio "80:20"
-    cli.ratio.should eq(0.8)
+    cli.ratio.should be_close(0.8, 1e-3)
   end
 
   it "Should parse sizes correctly" do
-    Charqui::SizeKB.parse("10mb").should eq(10 * 1024)
-    Charqui::SizeKB.parse("10Mb").should eq(10 * 1024)
-    Charqui::SizeKB.parse("10MB").should eq(10 * 1024)
-    Charqui::SizeKB.parse("10KB").should eq(10)
-    Charqui::SizeKB.parse("10kB").should eq(10)
-    Charqui::SizeKB.parse("1GB").should eq(1 * 1024 * 1024)
+    Charqui::SizeKB.new("10mb").value.should eq(10 * 1024)
+    Charqui::SizeKB.new("10Mb").value.should eq(10 * 1024)
+    Charqui::SizeKB.new("10MB").value.should eq(10 * 1024)
+    Charqui::SizeKB.new("1.5MB").value.should eq((1.5 * 1024).to_i)
+    Charqui::SizeKB.new("10KB").value.should eq(10)
+    Charqui::SizeKB.new("10kB").value.should eq(10)
+    Charqui::SizeKB.new("1GB").value.should eq(1 * 1024 * 1024)
+
+    # Parse decimal values.
+    Charqui::SizeKB.new("1.5MB").value.should eq((1.5 * 1024).to_i)
+
+    expect_raises Charqui::AppError do
+      # The string has to only have the string specifier.
+      Charqui::SizeKB.new("1.5MBz")
+    end
+
+    expect_raises Charqui::AppError do
+      Charqui::SizeKB.new("1..2MB")
+    end
+
+    expect_raises Charqui::AppError do
+      # The string *has* to contain the size specifier as the target size
+      # cannot be simply bytes, *for now*
+      Charqui::SizeKB.new("1234")
+    end
   end
 end
